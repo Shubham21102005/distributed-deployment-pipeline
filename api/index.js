56e3369c980f14c4b6bbcdd2f593e3bb27ef5d8f
Shubham <krshubham21102005@gmail.com>
2026-09-19 14:41:31 +0530

    built an api server that automates the process of spinning up container and starts a task for uploading to s3 bucket

---
+++ api/index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const { generateSlug } = require("random-word-slugs");
+const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs");
+
+const dotenv = require("dotenv");
+const PORT = 5000;
+const PROXY_PORT = process.env.PROXY_PORT || 8000;
+dotenv.config();
+const app = express();
+const config = {
+  CLUSTER: process.env.CLUSTER_ARN,
+  TASK: process.env.TASK_ARN,
+};
+const ecsClient = new ECSClient({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+app.use(express.json());
+
+app.post("/project", async (req, res) => {
+  const { gitURL } = req.body;
+  if (!gitURL) {
+    return res.status(400).json({ error: "gitURL is required" });
+  }
+  const projectSlug = generateSlug();
+  //spins the container
+  const command = new RunTaskCommand({
+    cluster: config.CLUSTER,
+    taskDefinition: config.TASK,
+    launchType: "FARGATE",
+    count: 1,
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        assignPublicIp: "ENABLED",
+        subnets: [
+          "subnet-02d2ccdc4139aebb1",
+          "subnet-0a7102664fbf39eaf",
+          "subnet-081c75b344309c9fa",
+        ],
+        securityGroups: ["sg-03114e2162838fde6"],
+      },
+    },
+    overrides: {
+      containerOverrides: [
+        {
+          name: "builder-image",
+          environment: [
+            { name: "GIT_REPOSITORY_URL", value: gitURL },
+            { name: "PROJECT_ID", value: projectSlug },
+            { name: "AWS_ACCESS_KEY", value: process.env.AWS_ACCESS_KEY },
+            {
+              name: "AWS_SECRET_ACCESS_KEY",
+              value: process.env.AWS_SECRET_ACCESS_KEY,
+            },
+          ],
+        },
+      ],
+    },
+  });
+  await ecsClient.send(command);
+
+  return res.json({
+    status: "queued",
+    data: {
+      projectSlug,
+      url: `http://${projectSlug}.localhost:${PROXY_PORT}`,
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`api server running on port ${PORT}`);
+});
